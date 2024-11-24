f2e8b494a84c8c900399df8d36c0566861ca267c
allanDev9 <allanpenaguerra2003@gmail.com>
2024-11-24 12:23:51 -0600

    to do

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -3,7 +3,7 @@ function agregarTarea() {
   let maxTareas = 3;
   let listaTareas = document.getElementById("listaTareas");
   let btnAgregar = document.getElementById("btn-agregar");
-
+  
   //Verificar si se ha alcanzado el limite de tareas
   if (listaTareas.children.length >= maxTareas) {
     btnAgregar.disabled = true; //Desactivar botón de agregar tareas cuando se llega al límite
@@ -12,44 +12,44 @@ function agregarTarea() {
   }
   let nuevaTareaTexto = document.getElementById("nuevaTarea").value;
   let textoInput = document.getElementById("nuevaTarea");
-
+  let botonAgregar = document.getElementById("btn-agregar");
+  let mensaje = document.getElementById("mensaje");
+  mensaje.style.color = "yellow";
+  mensaje.style.fontWeight = "bold";
+  
   //Validamos que la tarea no esté vacía
   if (nuevaTareaTexto === "" && nuevaTareaTexto <= 0 && nuevaTareaTexto >= 0) {
-    alert("Debes ingresar una tarea");
+    mensaje.innerHTML = "El campo no puede estar vacío";
     return;
-  } else if (nuevaTareaTexto <= 0 || nuevaTareaTexto >= 0) {
+  } else if (!isNaN(nuevaTareaTexto)) {
     alert("No se permiten numeros");
     textoInput.value = "";
     return;
   }
+
   // Verificar si la tarea ya existe en la lista
   for (let tarea of listaTareas.children) {
     if (tarea.textContent.replace("Eliminar", "").trim() === nuevaTareaTexto) {
-      alert("No puedes agregar una tarea igual a otra");
+      mensaje.innerHTML = "Esta tarea ya existe";
       return;
     }
   }
   //Crear elemento de la lista
   let nuevaTarea = document.createElement("li");
+
   nuevaTarea.textContent = nuevaTareaTexto + " ";
 
   //Crear botón de eliminar
   let botonEliminar = document.createElement("button");
   botonEliminar.textContent = "Eliminar";
-  botonEliminar.onclick = function () {
+  botonEliminar.addEventListener("click", () => {
     nuevaTarea.remove();
     //Si el numero de tareas es menor al limite, habilitamos el boton
     if (listaTareas.children.length < maxTareas) {
       btnAgregar.disabled = false;
     }
-  };
-  botonEliminar.style.color = "white";
-  botonEliminar.style.backgroundColor = "black";
-  botonEliminar.style.padding = "4px";
-  botonEliminar.style.borderRadius = "10px";
-  botonEliminar.style.position = "relative";
-  botonEliminar.style.minWidth = "60px";
-  botonEliminar.style.textAlign = "center";
+  });
+  botonEliminar.className = "boton-eliminar"; // Añadir clase para estilos
   //Agregar boton de eliminar de la lista
   nuevaTarea.appendChild(botonEliminar);
 
@@ -58,4 +58,12 @@ function agregarTarea() {
 
   //Limpiar el cuadro de texto del nombre de la tarea
   textoInput.value = "";
+
+  //Reiniciar el mensaje cuando se clickea en el input
+  textoInput.addEventListener("click", () => {
+    mensaje.innerHTML = "";
+  });
 }
+
+
+ 
